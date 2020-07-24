22ea3e30ce51ab437b50e17ddacd8abe42d3aa3f
BhavinShah12 <bhavinshah20@live.com>
2020-07-24 12:28:04 +0530

    Latest Commit

--- src/gitPackage/GitClass.java
+++ src/gitPackage/GitClass.java
@@ -2,9 +2,9 @@ package gitPackage;
 
 public class GitClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi This is XYZ from India");
 	}
 
 }
